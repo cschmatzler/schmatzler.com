a2ec10c2b6f745f316900675ecf45c269679406f
Christoph Schmatzler <christoph@medium.place>
2024-01-10 13:56:32 +0100

    WIP
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,8 +1,16 @@
-import { defineMarkdocConfig } from "@astrojs/markdoc/config";
+import { component, defineMarkdocConfig } from "@astrojs/markdoc/config";
 import shiki from "@astrojs/markdoc/shiki";
 import catppuccinLatte from "./src/shiki/catppuccin-latte.ts";
 
 export default defineMarkdocConfig({
+	tags: {
+		admonition: {
+			render: component("./src/components/Admonition.astro"),
+			attributes: {
+				title: { type: String },
+			}
+		}
+	},
 	extends: [
 		shiki({
 			theme: catppuccinLatte,
