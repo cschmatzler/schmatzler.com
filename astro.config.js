b856703be9e23696f4ecd000b5742e6300f12a0a
Christoph Schmatzler <christoph@medium.place>
2025-05-28 22:06:10 +0200

    up
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- astro.config.js
+++ astro.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 import catppuccinLatte from "./src/shiki/catppuccin-latte";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://schmatzler.com",
-  integrations: [tailwind(), sitemap(), mdx()],
-  devToolbar: {
-    enabled: false,
-  },
-  markdown: {
-    shikiConfig: {
-      theme: catppuccinLatte,
-    },
-  },
+	site: "https://schmatzler.com",
+	integrations: [sitemap(), mdx()],
+	vite: { plugins: [tailwindcss()] },
+	devToolbar: {
+		enabled: false,
+	},
+	markdown: {
+		shikiConfig: {
+			theme: catppuccinLatte,
+		},
+	},
 });
